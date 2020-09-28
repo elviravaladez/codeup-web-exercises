adbb49d2a1afbe85f1ccaa52d94bac48901e2a95
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-09-28 09:34:26 -0500

    created warmup files

---
+++ js/warmup.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log("Hello Vivian");
+console.log("Hello Sophia");
+console.log("Hello Samuel");
+console.log("Hello Brance");
+console.log("Hello Amber");
+
+function sayHello(name) {
+    console.log("Hello " + name);
+}
+
+sayHello("Vivian");
+sayHello("Sophia");
+sayHello("Samuel");
+sayHello("Brance");
+sayHello("Amber");
