e4db63f3dc1e56a04a3092bcae02c8b9e75b5d6e
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-02 17:00:42 -0500

    completed for loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -4,6 +4,9 @@
 //2 Create a function named showMultiplicationTable that accepts a number and console.logs
 // the multiplication table for that number (just multiply by the numbers 1 through 10)
 
+
+
+
 function showMultiplicationTable() {
     var num = 7;
     for(var i = 1; i <= 10; ++i) {
@@ -15,6 +18,7 @@ showMultiplicationTable();
 
 
 
+
 //3 Use a for loop and the code from the previous lessons to generate 10 random
 // numbers between 20 and 200 and output to the console whether each number is
 // odd or even. For example:
@@ -22,16 +26,14 @@ showMultiplicationTable();
 
 
 
-//var randomNumber= Math.floor((Math.random() * (200 - 20 + 1)) + 20);
-
-// for (var i = 0; i <= 10; i++) {
-//     var randomNumber= Math.floor((Math.random() * (200 - 20 + 1)) + 20);
-//     if(i % 2 === 0) {
-//         console.log(randomNumber + " is even.");
-//     } else {
-//         console.log(randomNumber + " is odd.");
-//     }
-// }
+for (var i = 0; i <= 9; i++) {
+    var randomNumber= Math.floor((Math.random() * (200 - 20 + 1)) + 20);
+    if(i % 2 === 0) {
+        console.log(randomNumber + " is even.");
+    } else {
+        console.log(randomNumber + " is odd.");
+    }
+}
 
 
 
@@ -41,19 +43,19 @@ showMultiplicationTable();
 
 
 
-// function numberPyramid() {
-//     var emptyString = " ";
-//     for(var i = 1; i <= 9; i++) {
-//
-//         for (var x = 1; x <= i; x++) {
-//
-//             emptyString += i + " "; // the empty string sets it up as the next row
-//         }
-//         console.log(emptyString);
-//         emptyString = " ";
-//     }
-// }
-// numberPyramid();
+function numberPyramid() {
+    var emptyString = " ";
+    for(var i = 1; i <= 9; i++) {
+
+        for (var x = 1; x <= i; x++) {
+
+            emptyString += i + " "; // the empty string sets it up as the next row
+        }
+        console.log(emptyString);
+        emptyString = " ";
+    }
+}
+numberPyramid();
 
 
 
@@ -62,8 +64,9 @@ showMultiplicationTable();
 //5 Create a for loop that uses console.log to create the output shown below.
 
 
-// for(var i = 100; i > 0; i--) {
-//     if(i % 5 === 0) {
-//         console.log(i);
-//     }
-// }
+
+for(var i = 100; i > 0; i--) {
+    if(i % 5 === 0) {
+        console.log(i);
+    }
+}
