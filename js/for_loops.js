5ef705c3c0f6e9757a49963a3b1eea79585507d5
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-02 16:48:55 -0500

    completed exercise 2-function showMultiplicationTable()

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,8 +1,26 @@
 "use strict";
 
+
+//2 Create a function named showMultiplicationTable that accepts a number and console.logs
+// the multiplication table for that number (just multiply by the numbers 1 through 10)
+
+function showMultiplicationTable() {
+    var num = 7;
+    for(var i = 1; i <= 10; ++i) {
+        console.log(num + "*" + i + "= " + num * i);
+    }
+}
+
+showMultiplicationTable();
+
+
+
 //3 Use a for loop and the code from the previous lessons to generate 10 random
 // numbers between 20 and 200 and output to the console whether each number is
 // odd or even. For example:
+
+
+
 
 //var randomNumber= Math.floor((Math.random() * (200 - 20 + 1)) + 20);
 
@@ -16,28 +34,36 @@
 // }
 
 
+
+
 //4 Create a for loop that uses console.log to create the output shown below.
-function numberPyramid() {
-    var totalNumberofRows = 9;
-    var emptyString = " ";
-    for(var i = 1; i <= totalNumberofRows; i++) {
 
-        for (var x = 1; x <= i; x++) {
 
-            emptyString += i + " "; // the empty string sets it up as the next row
-        }
-        console.log(emptyString);
-        emptyString = " ";
-    }
-}
-numberPyramid();
+
+
+// function numberPyramid() {
+//     var emptyString = " ";
+//     for(var i = 1; i <= 9; i++) {
+//
+//         for (var x = 1; x <= i; x++) {
+//
+//             emptyString += i + " "; // the empty string sets it up as the next row
+//         }
+//         console.log(emptyString);
+//         emptyString = " ";
+//     }
+// }
+// numberPyramid();
+
+
+
 
 
 //5 Create a for loop that uses console.log to create the output shown below.
 
-for(var i = 100; i > 0; i--) {
-    if(i % 5 === 0) {
-        console.log(i);
-    }
-}
 
+// for(var i = 100; i > 0; i--) {
+//     if(i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
