d82e5b15bb7c93c96ed93351b5a13a287cfffb46
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-02 16:18:49 -0500

    adding exercise 5

--- js/for_loops.js
+++ js/for_loops.js
@@ -6,11 +6,20 @@
 
 //var randomNumber= Math.floor((Math.random() * (200 - 20 + 1)) + 20);
 
-for (var i = 0; i <= 10; i++) {
-    var randomNumber= Math.floor((Math.random() * (200 - 20 + 1)) + 20);
-    if(i % 2 === 0) {
-        console.log(randomNumber + " is even.");
-    } else {
-        console.log(randomNumber + " is odd.");
+// for (var i = 0; i <= 10; i++) {
+//     var randomNumber= Math.floor((Math.random() * (200 - 20 + 1)) + 20);
+//     if(i % 2 === 0) {
+//         console.log(randomNumber + " is even.");
+//     } else {
+//         console.log(randomNumber + " is odd.");
+//     }
+// }
+
+//5 Create a for loop that uses console.log to create the output shown below.
+
+for(var i = 100; i > 0; i--) {
+    if(i % 5 === 0) {
+        console.log(i);
     }
 }
+
