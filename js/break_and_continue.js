f811a0ffc99d0e0f9de9e5918f8d06a18943f910
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-05 10:36:28 -0500

    adding while.js file and adding to break and continue.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,1 +1,26 @@
 "use strict";
+
+
+//Prompt the user for an odd number between 1 and 50. Use a loop
+// and a break statement to continue prompting the user
+// if they enter invalid input.
+
+//Use a loop and the continue statement to output all the odd numbers
+// between 1 and 50, except for the number the user entered.
+
+
+//prompt("Type in an odd number between 1 and 50");
+
+for (var i = 0; i < 50; i++) {
+    var oddNumber = prompt("Type in an odd number between 1 and 50");
+
+    if( 0 >= oddNumber > 50 || oddNumber % 2 === 0 || null) {
+        console.log('Number is less than 0, greater than 50, OR even');
+        prompt("Type in an odd number between 1 and 50");
+        continue;
+    } else {
+        console.log('Your number is ' + i );
+        alert('Your number is ' + i);
+        break;
+    }
+}
