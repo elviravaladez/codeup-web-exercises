1f6670a94d567c9ed0e1b0a17a5d1e888665be46
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-11-20 09:42:36 -0600

    Using object destructuring assignment

--- js/es6.js
+++ js/es6.js
@@ -71,8 +71,7 @@ let names = [];
 
 //new way
 users.forEach(user => emails.push(user.email));
-
-
+// console.log(emails); //would log all the emails including mine
 
 
 // //old way
@@ -83,17 +82,26 @@ users.forEach(user => emails.push(user.email));
 
 //new way
 users.forEach(user => names.push(user.name));
+// console.log(names); //would log all names including mine
 
 
 // TODO: replace `var` with `let` in the following declaration
 let developers = [];
-users.forEach(function(user) {
-  // TODO: rewrite the code below to use object destructuring assignment
-  //       note that you can also use destructuring assignment in the function
-  //       parameter definition
-  const name = user.name;
-  const email = user.email;
-  const languages = user.languages;
+
+
+//old way
+// users.forEach(function(user) {
+//   // TODO: rewrite the code below to use object destructuring assignment
+//   //       note that you can also use destructuring assignment in the function
+//   //       parameter definition
+//   const name = user.name;
+//   const email = user.email;
+//   const languages = user.languages;
+
+
+//new way
+  users.forEach(user => {
+    const {name, email, languages} = user;
 
   // TODO: rewrite the assignment below to use template strings
   developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
